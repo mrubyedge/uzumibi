42c3141a6f22aa8532c0beada03c351f151248f2
Uchio Kondo <udzura@udzura.jp>
2026-01-23 20:40:20 +0900

    Support fastly

--- uzumibi-on-fastly-spike/src/main.rs
+++ uzumibi-on-fastly-spike/src/main.rs
@@ -1,13 +1,13 @@
 // use fastly::http::StatusCode;
+extern crate anyhow;
+
+use anyhow::anyhow;
 use fastly::{Error, Request, Response};
-use uzumibi_on_fastly_spike::*;
+use uzumibi_on_fastly_spike as uzumibi;
 
 #[fastly::main]
 fn main(req: Request) -> Result<Response, Error> {
-    let sm = uzumibi_initialize_request(65536);
-    sm.borrow_mut().write(0, &pack_request_data(&req));
-
-    let res = uzumibi_start_request();
-
-    Ok(res)
+    uzumibi::uzumibi_initialize_request(req)
+        .map_err(|e| anyhow!("Failed to initialize request: {}", e))?;
+    uzumibi::uzumibi_start_request().map_err(|e| anyhow!("Failed to start request: {}", e))
 }
